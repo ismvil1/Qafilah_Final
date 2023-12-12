65d67864ae7e6296b871b7ff25cd6db97a79da4b
fahmedk <77476747+fahmedk@users.noreply.github.com>
2023-12-12 16:07:57 -0500

    Add files

---
+++ qafilah/item/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+
+from .forms import NewItemForm
+from .models import Item
+
+def detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
+
+
+    return render(request, 'item/detail.html', {
+        'item': item,
+        'related_items': related_items
+    })
+@login_required
+def new(req):
+    form = NewItemForm()
+
+    return render(req, 'item/form.html',{
+        'form': form
+    })
+
+
+
+# Create your views here.
