955b86c58ee980d98d1a4ac775677b6808589135
tort8678 <102439231+tort8678@users.noreply.github.com>
2023-12-14 18:07:29 -0500

    UI updates + Sign out button

--- qafilah/core/urls.py
+++ qafilah/core/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
+    path('signout', auth_views.LogoutView.as_view(template_name='core/base.html'), name='signout'),
     path('checkout/', views.checkout, name='checkout'),
     path('confirmation/',views.confirmation, name='confirmation')
 ]
